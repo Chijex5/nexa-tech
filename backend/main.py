94ce9eea834740fe9cdc981d4b60aa75fbdeb388
Chijioke Uzodinma <embroconnect3@gmail.com>
2026-05-31 23:20:58 +0100

    fix: correct import statement and update uvicorn run target

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
-import uvicorn
+from app import app
 
 if __name__ == "__main__":
+    import uvicorn
+
     uvicorn.run(
-        "app:app",
+        "main:app",
         host="0.0.0.0",
         port=8000,
         reload=True,
